5429cd2e4409ecc3371e83961750a9122d408e76
Alexander <a.thorik@yandex.ru>
2024-09-25 22:57:30 +0300

    feat: Добавлена фича кнопка для подключения к метамаск

---
+++ src/features/connectMetamask/index.ts
@@ -0,0 +1,1 @@
+export { ConnectMetamaskButton } from "./ui/connectMetamaskButton";
---
+++ src/features/connectMetamask/ui/connectMetamaskButton.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/shared/ui/button";
+import { FC } from "react";
+
+export const ConnectMetamaskButton: FC = () => {
+  return <Button>Connect metamask</Button>;
+};
