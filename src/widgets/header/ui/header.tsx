b6a890852da3f73796a2f6a64f431bf0fcb1ddf3
Alexander <a.thorik@yandex.ru>
2024-09-25 22:51:24 +0300

    feat: В виджет хэдера добавлена кнопка подключения к метамаск и слот для лого

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,9 +1,13 @@
+import { ConnectMetamaskButton } from "@/features";
 import { FC } from "react";
 
 export const Header: FC = () => {
   return (
     <header className="flex h-[72px] w-full items-center justify-between text-white">
-      Header
+      <div className="flex h-[44px] w-[199px] items-center justify-center border border-dashed border-[#C7C7C7] bg-[#5A5A5A]">
+        Logo
+      </div>
+      <ConnectMetamaskButton />
     </header>
   );
 };
