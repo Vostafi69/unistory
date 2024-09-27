42063dc64a0dbd560b2bc893b2fca3709f1f33df
Alexander <a.thorik@yandex.ru>
2024-09-27 15:23:32 +0300

    feat: добавлены анимации ревила объектов

--- src/pages/mainPage/ui/mainPage.tsx
+++ src/pages/mainPage/ui/mainPage.tsx
@@ -1,15 +1,16 @@
 import { RegistrBetaAccessForm, RoadmapStats } from "@/widgets";
 import { FC } from "react";
-import { Reveal } from "@/shared/lib/components/reveal";
+import { Reveal } from "@/shared/lib/framerMotion/components/reveal";
 import { Planet } from "@/shared/ui/planet";
+import { motion } from "framer-motion";
 
 export const MainPage: FC = () => {
   return (
-    <>
+    <div>
       <section className="relative flex min-h-[628px] flex-row items-end justify-between pb-[60px]">
         <div className="flex w-full items-start justify-between">
           <div className="relative space-y-[42px]">
-            <div className="relative select-none">
+            <motion.div className="relative select-none">
               <h1 className="relative z-10 font-BebasNeue text-[120px] font-bold leading-none">
                 <Reveal delay={0.25}>Explore Your own planet</Reveal>
                 <Reveal delay={0.45}>
@@ -20,7 +21,9 @@ export const MainPage: FC = () => {
                   metaverse
                 </Reveal>
               </h1>
-              <div className="absolute left-0 top-0 z-30 !mt-0 bg-transparent font-BebasNeue text-[120px] font-bold leading-none text-primary [mask-image:_url('/mars.svg')] [mask-position:_right_24px_center] [mask-repeat:_no-repeat] [mask-size:_322px]">
+              <motion.div
+                className={`absolute left-0 top-0 z-30 !mt-0 bg-transparent font-BebasNeue text-[120px] font-bold leading-none text-primary [mask-image:_url('/mars.svg')] [mask-position:_right_24px_center] [mask-repeat:_no-repeat] [mask-size:_322px]`}
+              >
                 <Reveal delay={0.25}>Explore Your own planet</Reveal>
                 <Reveal delay={0.45}>
                   In{" "}
@@ -29,10 +32,10 @@ export const MainPage: FC = () => {
                   </span>{" "}
                   metaverse
                 </Reveal>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
             <Planet
-              className="absolute -top-[41px] right-[25px] select-none"
+              className="absolute -top-[41px] right-[25px] !mt-0 select-none"
               planetBadge={<Planet.Badge />}
               planetRings={<Planet.Rings />}
             />
@@ -52,20 +55,24 @@ export const MainPage: FC = () => {
       </section>
       <section className="pb-[86px] pt-12">
         <div className="w-[421px]">
-          <h2 className="font-BebasNeue text-3xl font-bold text-primary">
-            Beta test registration
-          </h2>
-          <p className="mt-[18px] font-AvenirNextCyr">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
+          <Reveal>
+            <h2 className="font-BebasNeue text-3xl font-bold text-primary">
+              Beta test registration
+            </h2>
+          </Reveal>
+          <Reveal delay={0.25}>
+            <p className="mt-[18px] font-AvenirNextCyr">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Reveal>
           <div className="mt-9">
             <RegistrBetaAccessForm />
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 };
