a1e1fd71900c1a3d95099b041c84db957e0173b5
Alexander <a.thorik@yandex.ru>
2024-09-27 17:47:31 +0300

    feat: Добавлен BASE_URL и теги для инвалидации

--- src/shared/lib/api.ts
+++ src/shared/lib/api.ts
@@ -1,6 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  tagTypes: ["Users"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
   endpoints: () => ({}),
 });
