9672fbaf0a85af8e320dc1d108c9051b9de09802
Alexander <a.thorik@yandex.ru>
2024-09-27 12:17:05 +0300

    fix: Исправлены значения направления у анимации

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -5,12 +5,12 @@ import { FC } from "react";
 export const Header: FC = () => {
   return (
     <header className="flex h-[72px] w-full items-center justify-between text-white">
-      <Reveal direction="bottom" delay={0.25}>
+      <Reveal direction={-1} delay={0.25}>
         <div className="flex h-[44px] w-[199px] items-center justify-center border border-dashed border-[#C7C7C7] bg-[#5A5A5A]">
           Logo
         </div>
       </Reveal>
-      <Reveal direction="bottom" delay={0.45}>
+      <Reveal direction={-1} delay={0.45}>
         <ConnectMetamaskButton />
       </Reveal>
     </header>
