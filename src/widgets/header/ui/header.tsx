1aadd3bbfacd08a354ccab7fc98936fb51cf414c
Alexander <a.thorik@yandex.ru>
2024-09-27 11:55:07 +0300

    feat: Добавлена анимация появления элементов шапки

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,13 +1,18 @@
 import { ConnectMetamaskButton } from "@/features";
+import { Reveal } from "@/shared/lib/components/reveal";
 import { FC } from "react";
 
 export const Header: FC = () => {
   return (
     <header className="flex h-[72px] w-full items-center justify-between text-white">
-      <div className="flex h-[44px] w-[199px] items-center justify-center border border-dashed border-[#C7C7C7] bg-[#5A5A5A]">
-        Logo
-      </div>
-      <ConnectMetamaskButton />
+      <Reveal direction="bottom" delay={0.25}>
+        <div className="flex h-[44px] w-[199px] items-center justify-center border border-dashed border-[#C7C7C7] bg-[#5A5A5A]">
+          Logo
+        </div>
+      </Reveal>
+      <Reveal direction="bottom" delay={0.45}>
+        <ConnectMetamaskButton />
+      </Reveal>
     </header>
   );
 };
