b4a2a44af7cf1ec80a1df3f25dfe6d6f3a362d8b
Alexander <a.thorik@yandex.ru>
2024-09-28 22:28:45 +0300

    feat: Сделана страница участника

--- src/pages/memberPage/ui/memberPage.tsx
+++ src/pages/memberPage/ui/memberPage.tsx
@@ -1,5 +1,20 @@
+import { UserCard } from "@/entities";
+import { Planet } from "@/shared/ui/planet";
 import { FC } from "react";
 
 export const MemberPage: FC = () => {
-  return <>Member Page</>;
+  return (
+    <section className="relative flex flex-row justify-between">
+      <div className="mt-[96px] space-y-[48px]">
+        <h1 className="font-BebasNeue text-5xl font-bold leading-none">
+          PERSONAL DATA
+        </h1>
+        <UserCard email="123" name="123" wallet="123" />
+      </div>
+      <Planet
+        className="absolute -right-[160px] top-[209px] !mt-0 select-none"
+        planetRings={<Planet.Rings />}
+      />
+    </section>
+  );
 };
