3f387e12fdc2bb62d55e93413e744249a2fe5d07
Alexander <a.thorik@yandex.ru>
2024-09-28 15:16:52 +0300

    feat: Добавлена возможность отображать кошелек вместо кнопки

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,8 +1,11 @@
 import { ConnectMetamaskButton } from "@/features";
 import { Reveal } from "@/shared/lib/framerMotion/components/reveal";
 import { FC } from "react";
+import { useAccount } from "wagmi";
 
 export const Header: FC = () => {
+  const { address } = useAccount();
+
   return (
     <header className="flex h-[72px] w-full items-center justify-between text-white">
       <Reveal direction={-1} delay={0.25}>
@@ -11,7 +14,13 @@ export const Header: FC = () => {
         </div>
       </Reveal>
       <Reveal direction={-1} delay={0.45}>
-        <ConnectMetamaskButton />
+        {address ? (
+          <p className="max-w-[145px] truncate font-BebasNeue text-lg font-bold uppercase text-primary">
+            {address}
+          </p>
+        ) : (
+          <ConnectMetamaskButton />
+        )}
       </Reveal>
     </header>
   );
