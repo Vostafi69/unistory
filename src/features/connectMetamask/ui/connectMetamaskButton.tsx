6d0a8d6cef83263b61dae5f10e89482fbb2fb846
Alexander <a.thorik@yandex.ru>
2024-09-28 15:16:18 +0300

    feat: Доработана фича присоединения к metamask

--- src/features/connectMetamask/ui/connectMetamaskButton.tsx
+++ src/features/connectMetamask/ui/connectMetamaskButton.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/shared/ui/button";
 import { FC } from "react";
+import { useConnect } from "wagmi";
 
 export const ConnectMetamaskButton: FC = () => {
-  return <Button>Connect metamask</Button>;
+  const { connectors, connect } = useConnect();
+
+  const handleConnect = () => {
+    connect({ connector: connectors[0] });
+  };
+
+  return <Button onClick={handleConnect}>Connect {connectors[0].name}</Button>;
 };
