ea0275b773006e5efacea42eeb28f933ee717530
Alexander <a.thorik@yandex.ru>
2024-09-29 15:14:01 +0300

    feat: Добавлен ручной рендер outlet для анимации

--- src/app/layouts/mainLayout.tsx
+++ src/app/layouts/mainLayout.tsx
@@ -1,17 +1,25 @@
-import { FC, ReactNode } from "react";
-import { Outlet } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { cloneElement, FC, ReactNode } from "react";
+import { useLocation, useOutlet } from "react-router-dom";
 
 interface MainLayoutProps {
   headerSlot?: ReactNode;
 }
 
 export const MainLayout: FC<MainLayoutProps> = ({ headerSlot }) => {
+  const location = useLocation();
+  const element = useOutlet();
+
+  console.log(element);
+
   return (
-    <div className="min-h-screen w-full bg-background px-16 text-base-color">
+    <div className="min-h-screen w-full overflow-x-hidden bg-background px-16 text-base-color">
       <div className="mx-auto max-w-[1312px]">
         {headerSlot}
         <main>
-          <Outlet />
+          <AnimatePresence mode="wait">
+            {element && cloneElement(element, { key: location.pathname })}
+          </AnimatePresence>
         </main>
       </div>
     </div>
