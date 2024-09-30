98b7276b0c3336c44810375c300d69964c7809d1
Alexander <a.thorik@yandex.ru>
2024-09-30 11:19:04 +0300

    feat: Добавлены состояния кнопки

--- src/features/connectMetamask/ui/connectMetamaskButton.tsx
+++ src/features/connectMetamask/ui/connectMetamaskButton.tsx
@@ -3,11 +3,17 @@ import { FC } from "react";
 import { useConnect } from "wagmi";
 
 export const ConnectMetamaskButton: FC = () => {
-  const { connectors, connect } = useConnect();
+  const { connectors, connect, status } = useConnect();
 
   const handleConnect = () => {
     connect({ connector: connectors[0] });
   };
 
-  return <Button onClick={handleConnect}>Connect {connectors[0].name}</Button>;
+  return (
+    <Button onClick={handleConnect}>
+      {status === "pending" && "Connecting..."}
+      {status === "idle" && "Connect " + connectors[0].name}
+      {status === "error" && "Connection error"}
+    </Button>
+  );
 };
