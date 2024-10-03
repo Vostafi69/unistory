a761e91e5a72b8cce8e86fb72c8d900b66b2dce2
Alexander Thorik <a.thorik@yandex.ru>
2024-10-03 14:06:09 +0300

    fix: Исправлен border у инпутов

--- src/shared/ui/input.tsx
+++ src/shared/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-[42px] w-full rounded-[30px] border border-input bg-transparent px-[18px] py-3 font-AvenirNextCyr transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:border-primary focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full rounded-[30px] border border-input bg-transparent px-[18px] py-[8.1px] font-AvenirNextCyr transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:border-primary focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
